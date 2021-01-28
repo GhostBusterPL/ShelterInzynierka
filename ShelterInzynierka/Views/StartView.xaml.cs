5346a4b6cb456a43dba5a0e05cf457592af8774a
InternetowyRewolwerowiec <emilo400@vp.pl>
2021-01-28 22:34:22 +0100

    Add StartView - all GUI

--- ShelterInzynierka/Views/StartView.xaml.cs
+++ ShelterInzynierka/Views/StartView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ShelterInzynierka
+namespace ShelterInzynierka.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for StartView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class StartView : Window
     {
-        public MainWindow()
+        public StartView()
         {
             InitializeComponent();
         }
