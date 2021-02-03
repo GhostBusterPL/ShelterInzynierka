bb01b00477833c1a46c2a3d768842238b44389e1
InternetowyRewolwerowiec <emilo400@vp.pl>
2021-02-03 11:58:54 +0100

    Add Dog List GUI

---
+++ ShelterInzynierka/Views/AdopterAdd.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ShelterInzynierka.Views
+{
+    /// <summary>
+    /// Interaction logic for AdopterAdd.xaml
+    /// </summary>
+    public partial class AdopterAdd : Window
+    {
+        public AdopterAdd()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_Save(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_Back(object sender, RoutedEventArgs e)
+        {
+            var newWindow = new StartView();
+            Close();
+            newWindow.Close();
+        }
+    }
+}
